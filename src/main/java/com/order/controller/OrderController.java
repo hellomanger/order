74264ef7f581cd9ec7e700d096611f40e371744a
N-264 <N-264@chengzhiyong>
2018-02-02 17:46:54 +0800

    静态资源

--- src/main/java/com/order/controller/OrderController.java
+++ src/main/java/com/order/controller/OrderController.java
@@ -1,17 +1,24 @@
 package com.order.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/order")
+import com.order.domain.UserDO;
+import com.order.service.UserService;
+
+@RequestMapping()
 @Controller
 public class OrderController {
 	
-	@RequestMapping("/index")
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping()
 	String welcome(Model model) {
+		//UserDO user = userService.get(1);
+		//System.out.println(user.getName());
 		return "index";
 	}
 }
